24fdb4ce56bd770aabd52ac86e8b62b2e4eeff6c
pranshumishra180-ops <pranshumishra180@gmail.com>
2025-10-30 12:02:29 +0530

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,7 @@
+ let link = document.querySelector("#links");
+
+ link.addEventListener("click", function(){
+    link.style.color = "green";
+    link.textContent = "price 170000 ";
+    // link.textContent = "discount 160000"
+ })
